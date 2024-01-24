1a8adb987ab5806904e6aa476dd7dafbb6afe0c1
Keith Camilleri <deliverkeith@outlook.com>
2024-01-24 23:36:33 +0100

    Updated menu and date of php release date format

---
+++ Unit1/sc-bed-simplephpsite-starter/configuration.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<?php include 'includes/head.php' ?>
+
+<body>
+    <?php include 'includes/menu.php' ?>
+
+    <?php 
+    echo phpinfo();
+    ?>
+    
+</body>
+
+</html>
--- Unit1/sc-bed-simplephpsite-starter/index.php
+++ Unit1/sc-bed-simplephpsite-starter/index.php
@@ -1,6 +1,7 @@
 <?php
     $phpReleaseDate = new DateTime('1995-06-08');
     $phpAge = $phpReleaseDate->diff(new DateTime());
+    $currentDate = new DateTime();
 ?>
 
 <!doctype html>
@@ -12,9 +13,11 @@
     <div class="container">
         <h1>Learning PHP</h1>
         <p>Welcome to the <strong>Learning PHP</strong> home page!</p>
-        <p>Select a link above to start learning. about a language with <?=$phpAge->y?> years of heritage!</p>
+        <p>Select a link above to start learning. about a language with <?=$phpAge->y?> years and <?=$phpAge->m?> months of heritage!</p>
     </div>
     
+    <?php echo 'PHP Release Date: ' . $phpReleaseDate->format('Y-m-d') . '<br>';?>
+    <?php echo 'Current Date: ' . $currentDate->format('Y-m-d') . '<br>'; ?>
 </body>
 
 </html>
